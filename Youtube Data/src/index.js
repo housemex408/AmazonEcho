d017793ab35b219e0bc021316827a7e069c2ee25
Jose Alvarado <jalvarado80@gmail.com>
2017-01-16 17:20:51 -0800

    new templating

--- Youtube Data/src/index.js
+++ Youtube Data/src/index.js
@@ -4,61 +4,114 @@ var appId = ''; //'amzn1.echo-sdk-ams.app.your-skill-id';
 
 // https://developer.amazon.com/blogs/post/Tx24Z2QZP5RRTG1/new-alexa-technical-tutorial-debugging-aws-lambda-code-locally
 
-exports.handler = (event, context) => {
+exports.handler = function (event, context) {
     try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
 
-        if(event.session.new)
-        {
-            console.log("NEW SESSION")
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+
+//     if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.05aecccb3-1461-48fb-a008-822ddrt6b516") {
+//         context.fail("Invalid Application ID");
+//      }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
         }
 
-        switch (event.request.type) {
-
-            case "LaunchRequest":
-                console.log("LAUNCH REQUEST")
-                context.succeed(
-                    generateResponse(
-                        buildSpeechletResponse("Welcome to an Alexa skill, this is running on a deployed lambda function", true),
-                        {}
-                    )
-                )
-                break;
-
-            case "IntentRequest":
-                console.log("INTENT REQUEST")
-                break;
-
-            case "SessionEndedRequest":
-                console.log("SESSION ENDED REQUEST")
-                break;
-
-            default:
-                context.fail(`INVALID REQUEST TYPE: $(event.request.type)`)
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
         }
-
-    } catch (error) {
-        context.fail(`EXCEPTION: $(error)`)
+    } catch (e) {
+        context.fail("Exception: " + e);
     }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
 }
 
-const buildSpeechletResponse = (outputText, shouldEndSession) => {
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
 
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // Add any cleanup logic here
+}
+
+var CARD_TITLE = "Youtube data"; // Be sure to change this for your skill.
+
+function getWelcomeResponse(callback) {
+    var sessionAttributes = {},
+        speechOutput = "Welcome to an Alexa skill, this is running on a deployed lambda function",
+        shouldEndSession = true
+
+    callback(sessionAttributes,
+        buildSpeechletResponse(CARD_TITLE, speechOutput, shouldEndSession));
+}
+
+// ------- Helper functions to build responses -------
+
+
+function buildSpeechletResponse(title, output, shouldEndSession) {
     return {
         outputSpeech: {
             type: "PlainText",
-            text: outputText
+            text: output
         },
         shouldEndSession: shouldEndSession
-    }
-
+    };
 }
 
-const generateResponse = (speechletResponse, sessionAttributes) => {
-
+function buildResponse(sessionAttributes, speechletResponse) {
     return {
         version: "1.0",
         sessionAttributes: sessionAttributes,
         response: speechletResponse
-    }
-
+    };
 }
